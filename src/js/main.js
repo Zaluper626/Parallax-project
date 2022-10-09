225bf4f8999bd484eeec5d186819e65a9f61ff7f
Zaluper626 <kmelnik999@gmail.com>
2022-10-10 01:33:33 +0300

    final stage of the project

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import "../../node_modules/focus-visible/dist/focus-visible"
-import $ from 'jquery';
 import "../scss/main.scss";
 import html from "../index.html";
 
-$('body').css('color', 'red');
-
-
-
+document.addEventListener('scroll', e => {
+    document.body.style.cssText = `--scrollTop: ${window.scrollY}px`;
+});
