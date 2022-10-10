6491da2ffd759cd55dc065c995abb552e8d84f1c
Zaluper626 <kmelnik999@gmail.com>
2022-10-10 19:51:54 +0300

    the project is completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import "../../node_modules/focus-visible/dist/focus-visible"
 import "../scss/main.scss";
 import html from "../index.html";
+import "../library/gsap/gsap.min.js";
+import "../library/gsap/ScrollTrigger.min.js";
+import "../library/gsap/ScrollSmoother.min.js";
 
-document.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${window.scrollY}px`;
+document.addEventListener('scroll', () => {
+    document.body.style.setProperty(`--scrollTop`, `${window.scrollY}px`);
 });
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+});
+
